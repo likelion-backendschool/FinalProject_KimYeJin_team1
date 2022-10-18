b87f92872784b2d20b179362ae84948aa4f8ce95
kyj2212 <kyj2212@gmail.com>
2022-10-18 16:45:42 +0900

    Chore: init project

---
+++ 1Week_Mission/src/main/java/com/yejin/exam/wbook/WbookApplication.java
@@ -0,0 +1,13 @@
+package com.yejin.exam.wbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbookApplication.class, args);
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+<br />
+<div align="center">
+
+  <h3 align="center">web ebook</h3>
+
+  <p align="center">
+      멋쟁이 사자처럼 종합프로젝트 wbook 만들기   
+    <br />
+  </p>
+</div>
+
+### 코드 컨밴션
+
+### Build 
+
+### TDD
