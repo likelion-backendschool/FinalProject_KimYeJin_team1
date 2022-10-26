cee89cd80fc02201da7ad4ee01cca41b274e40f4
kyj2212 <kyj2212@gmail.com>
2022-10-26 16:00:49 +0900

    Feat: cartService

---
+++ 2Week_Mission/src/main/java/com/yejin/exam/wbook/domain/cart/service/CartService.java
@@ -0,0 +1,23 @@
+package com.yejin.exam.wbook.domain.cart.service;
+
+import com.yejin.exam.wbook.domain.cart.entity.CartItem;
+import com.yejin.exam.wbook.domain.member.entity.Member;
+import com.yejin.exam.wbook.domain.product.entity.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final CartItemRepository cartItemRepository;
+
+    public void addItem(Member member, Product product, int quantity) {
+        CartItem cartItem = CartItem.builder()
+                .member(member)
+                .product-(product)
+                .quantity(quantity)
+                .build();
+
+        cartItemRepository.save(cartItem);
+    }
+}
