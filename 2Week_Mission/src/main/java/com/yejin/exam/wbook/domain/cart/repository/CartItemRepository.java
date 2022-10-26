93ff4b89cfba04838a2a1d2c2de65afb6f06008e
kyj2212 <kyj2212@gmail.com>
2022-10-26 17:07:12 +0900

    Feat: remove Item in cart

--- 2Week_Mission/src/main/java/com/yejin/exam/wbook/domain/cart/repository/CartItemRepository.java
+++ 2Week_Mission/src/main/java/com/yejin/exam/wbook/domain/cart/repository/CartItemRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> findByMemberIdAndProductOptionId(long memberId, long productOptionId);
     List<CartItem> findAllByMemberId(Long memberId);
+    boolean existsByMemberIdAndProductId(long memberId, long productId);
+
+    Optional<CartItem> findByMemberIdAndProductId(long buyerId, long productId);
 }
--- 2Week_Mission/src/main/java/com/yejin/exam/wbook/domain/cart/service/CartService.java
+++ 2Week_Mission/src/main/java/com/yejin/exam/wbook/domain/cart/service/CartService.java
@@ -3,14 +3,17 @@ package com.yejin.exam.wbook.domain.cart.service;
 import com.yejin.exam.wbook.domain.cart.entity.CartItem;
 import com.yejin.exam.wbook.domain.cart.repository.CartItemRepository;
 import com.yejin.exam.wbook.domain.member.entity.Member;
+import com.yejin.exam.wbook.domain.product.entity.Product;
 import com.yejin.exam.wbook.domain.product.entity.ProductOption;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CartService {
     private final CartItemRepository cartItemRepository;
 
@@ -38,5 +41,29 @@ public class CartService {
 
     public void deleteItem(CartItem cartItem) {
         cartItemRepository.delete(cartItem);
+    }
+
+    public boolean hasItem(Member member, Product product) {
+        return cartItemRepository.existsByMemberIdAndProductId(member.getId(), product.getId());
+    }
+    @Transactional
+    public boolean removeItem(Member member, Product product) {
+        CartItem oldCartItem = cartItemRepository.findByMemberIdAndProductId(member.getId(), product.getId()).orElse(null);
+        if (oldCartItem != null) {
+            cartItemRepository.delete(oldCartItem);
+            return true;
+        }
+        return false;
+    }
+    public void removeItem(CartItem cartItem) {
+        cartItemRepository.delete(cartItem);
+    }
+
+    public void removeItem(
+            Member member,
+            Long productId
+    ) {
+        Product product = new Product(productId);
+        removeItem(member, product);
     }
 }
