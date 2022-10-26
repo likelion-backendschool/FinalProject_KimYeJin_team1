e2b87669796927f557ebdccdae42580aef9d0a1e
kyj2212 <kyj2212@gmail.com>
2022-10-26 16:06:29 +0900

    Feat: add cart item sample data

--- 2Week_Mission/src/main/java/com/yejin/exam/wbook/global/base/init/DevInitData.java
+++ 2Week_Mission/src/main/java/com/yejin/exam/wbook/global/base/init/DevInitData.java
@@ -1,5 +1,6 @@
 package com.yejin.exam.wbook.global.base.init;
 
+import com.yejin.exam.wbook.domain.cart.service.CartService;
 import com.yejin.exam.wbook.domain.member.dto.MemberDto;
 import com.yejin.exam.wbook.domain.member.entity.Member;
 import com.yejin.exam.wbook.domain.member.service.MemberService;
@@ -16,14 +17,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Profile("dev")
 public class DevInitData {
     @Bean
-    CommandLineRunner initData(MemberService memberService, PostService postService, PasswordEncoder passwordEncoder, ProductService productService) {
+    CommandLineRunner initData(MemberService memberService, PostService postService, PasswordEncoder passwordEncoder, ProductService productService, CartService cartService) {
         return args -> {
 
             Member member1=memberService.join(new MemberDto("user1","1234","1234","kyj011202@naver.com","author1"));
             for(int i =1;i<=10;i++){
                 postService.write(member1,"제목%d".formatted(i),"내용%d".formatted(i),"내용%d".formatted(i),"#태그%d #태그%d".formatted(i,i+1));
             }
-            productService.create(member1,"도서제목1",1234,1L,"#도서태그1 #도서태그2");
+            for(int i=1;i<=5;i++){
+
+                productService.create(member1,"도서제목%d".formatted(i),1234,(long)i,"#도서태그%d #도서태그%d".formatted(i,i+1));
+            }
+
+            cartService.addItem(member1, productService.findById(0L).get(), 1);
+            cartService.addItem(member1, productService.findById(1L).get(), 4);
+            cartService.addItem(member1, productService.findById(2L).get(), 2);
 
         };
     }
