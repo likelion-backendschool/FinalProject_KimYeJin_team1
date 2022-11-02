15b3cea6b31138061cc3839e7e2e86795309aa60
kyj2212 <kyj2212@gmail.com>
2022-11-02 16:48:42 +0900

    Feat: withdraw entity

--- 3Week_Mission/src/main/java/com/yejin/exam/wbook/domain/withdraw/entity/Withdraw.java
+++ 3Week_Mission/src/main/java/com/yejin/exam/wbook/domain/withdraw/entity/Withdraw.java
@@ -1,2 +1,27 @@
-package com.yejin.exam.wbook.domain.withdraw.entity;public class Withdraw {
+package com.yejin.exam.wbook.domain.withdraw.entity;
+
+import com.yejin.exam.wbook.global.base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+@Table(name = "withdraw")
+public class Withdraw extends BaseEntity {
+    private String bankName;
+    private String backAccountNo;
+    private int price;
+    private boolean isApplied; // 신청여부
+    private boolean isCanceled; // 취소여부
+    private boolean isPaid; // 출금처리여부(지급여부)
 }
