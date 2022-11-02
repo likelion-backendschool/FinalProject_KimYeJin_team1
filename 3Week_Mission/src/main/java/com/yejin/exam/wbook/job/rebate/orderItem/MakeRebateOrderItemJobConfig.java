ecfc19660598310ba4bf1792843cc012a93739c2
kyj2212 <kyj2212@gmail.com>
2022-11-02 12:04:24 +0900

    Fix: repositoryItemReader -> jpaPagingItemReader

--- 3Week_Mission/src/main/java/com/yejin/exam/wbook/job/rebate/orderItem/MakeRebateOrderItemJobConfig.java
+++ 3Week_Mission/src/main/java/com/yejin/exam/wbook/job/rebate/orderItem/MakeRebateOrderItemJobConfig.java
@@ -13,11 +13,14 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -26,6 +29,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
@@ -34,20 +38,24 @@ import java.util.Collections;
 @RequiredArgsConstructor
 @Slf4j
 public class MakeRebateOrderItemJobConfig {
+
+    public static final String JOB_NAME = "makeRebateOrderItem";
+
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-
+    private final EntityManagerFactory emf;
     private final OrderItemRepository orderItemRepository;
     private final RebateOrderItemRepository rebateOrderItemRepository;
 
-    @Bean
+    @Bean(JOB_NAME+"Job")
     //    @Scheduled(cron= "0 0 4 15 * ?"  )
-    @Scheduled(cron= "0 1 * * * ?"  )
+//    @Scheduled(cron= "0 1 * * * ?"  )
     public Job makeRebateOrderItemJob(Step makeRebateOrderItemStep1, CommandLineRunner initData) throws Exception {
         initData.run();
         log.debug("[rebateJob] start");
-        return jobBuilderFactory.get("makeRebateOrderItemJob")
+        return jobBuilderFactory.get(JOB_NAME+"Job")
                 .start(makeRebateOrderItemStep1)
+                .incrementer(new RunIdIncrementer())
 //                .start(step2(null))
                 .build();
     }
@@ -62,26 +70,28 @@ public class MakeRebateOrderItemJobConfig {
 //                })
 //                .build();
 //    }
-    @Bean
+    @Bean(JOB_NAME+"Step1")
     @JobScope
     public Step makeRebateOrderItemStep1(
             ItemReader orderItemReader,
             ItemProcessor orderItemToRebateOrderItemProcessor,
             ItemWriter rebateOrderItemWriter
     ) {
-        return stepBuilderFactory.get("makeRebateOrderItemStep1")
-                .<OrderItem, RebateOrderItem>chunk(100)
+        log.debug("[rebateJob] step1 start");
+        Step step = stepBuilderFactory.get(JOB_NAME+"Step1")
+                .<OrderItem, RebateOrderItem>chunk(5)
                 .reader(orderItemReader)
                 .processor(orderItemToRebateOrderItemProcessor)
                 .writer(rebateOrderItemWriter)
                 .build();
+        log.debug("[step] processor : "+step.toString());
+        return step;
     }
-
     @StepScope
-    @Bean
-    public RepositoryItemReader<OrderItem> orderItemReader(
-            @Value("#{jobParameters['month']}") String yearMonth
-    ) {
+    @Bean("orderItemReader")
+    public JpaPagingItemReader<OrderItem> orderItemReader(@Value("#{jobParameters['month']}") String yearMonth) throws Exception{
+
+        log.debug("[rebateJob] reader start");
         if(yearMonth==null){
             yearMonth = "2022-11";
         }
@@ -90,26 +100,66 @@ public class MakeRebateOrderItemJobConfig {
         LocalDateTime fromDate = Util.date.parse(yearMonth + "-01 00:00:00.000000");
         LocalDateTime toDate = Util.date.parse(yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay));
 
-        return new RepositoryItemReaderBuilder<OrderItem>()
+        log.debug("[batch] findAllByPayDateBetween : ");
+//        RepositoryItemReader<OrderItem> repositoryItemReader = new RepositoryItemReaderBuilder<OrderItem>()
+//                .name(JOB_NAME+"reader")
+//                .repository(orderItemRepository)
+//                .methodName("findAllByPayDateBetween")
+//                .pageSize(5)
+//                .arguments(Arrays.asList(fromDate, toDate))
+//                .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+//                .build();
+
+        return new JpaPagingItemReaderBuilder<OrderItem>()
                 .name("orderItemReader")
-                .repository(orderItemRepository)
-                .methodName("findAllByPayDateBetween")
-                .pageSize(100)
-                .arguments(Arrays.asList(fromDate, toDate))
-                .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+                .entityManagerFactory(emf)
+                .pageSize(5)
+                .queryString("SELECT o FROM OrderItem as o")
                 .build();
     }
+
+//    @StepScope
+//    @Bean("orderItemReader")
+//    public RepositoryItemReader<OrderItem> orderItemReader(
+//            @Value("#{jobParameters['month']}") String yearMonth
+//    ) throws Exception {
+//        log.debug("[rebateJob] reader start");
+//        if(yearMonth==null){
+//            yearMonth = "2022-11";
+//        }
+////        int monthEndDay = Util.date.getEndDayOf(jobParameter.getCreateDate());
+//        int monthEndDay = Util.date.getEndDayOf(yearMonth);
+//        LocalDateTime fromDate = Util.date.parse(yearMonth + "-01 00:00:00.000000");
+//        LocalDateTime toDate = Util.date.parse(yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay));
+//
+//        log.debug("[batch] findAllByPayDateBetween : ");
+//        RepositoryItemReader<OrderItem> repositoryItemReader = new RepositoryItemReaderBuilder<OrderItem>()
+//                .name(JOB_NAME+"reader")
+//                .repository(orderItemRepository)
+//                .methodName("findAllByPayDateBetween")
+//                .pageSize(5)
+//                .arguments(Arrays.asList(fromDate, toDate))
+//                .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+//                .build();
+////        log.debug("[batch] respositoryReader : " + repositoryItemReader.read().getId());
+//        return repositoryItemReader;
+//    }
 
     @StepScope
     @Bean
     public ItemProcessor<OrderItem, RebateOrderItem> orderItemToRebateOrderItemProcessor() {
+        log.debug("[rebateJob] processor start");
         return orderItem -> new RebateOrderItem(orderItem);
     }
 
     @StepScope
-    @Bean
+    @Bean("rebateOrderItemWriter")
     public ItemWriter<RebateOrderItem> rebateOrderItemWriter() {
+        log.debug("[rebateJob] writer start");
         return items -> items.forEach(item -> {
+            log.debug("[batch] getOrderItem : "+item.getOrderItem().getProduct().getSubject());
+            log.debug("[batch] getOrderItem id : "+item.getOrderItem().getId());
+            log.debug("[batch] rebate Item paydate : "+item.getPayDate());
             RebateOrderItem oldRebateOrderItem = rebateOrderItemRepository.findByOrderItemId(item.getOrderItem().getId()).orElse(null);
 
             if (oldRebateOrderItem != null) {
