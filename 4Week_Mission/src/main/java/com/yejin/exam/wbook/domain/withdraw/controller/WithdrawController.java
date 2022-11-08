4446e58b1a9286fca35ca9568a4e3a253be9d1b8
kyj2212 <kyj2212@gmail.com>
2022-11-08 16:47:36 +0900

    #33 Fix: add fail msg

--- 4Week_Mission/src/main/java/com/yejin/exam/wbook/domain/withdraw/controller/WithdrawController.java
+++ 4Week_Mission/src/main/java/com/yejin/exam/wbook/domain/withdraw/controller/WithdrawController.java
@@ -15,10 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
